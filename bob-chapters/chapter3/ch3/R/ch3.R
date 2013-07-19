b1bfb70cbbd8045ac2f1a3a4b81f33e9efef5849
Jay Jacobs <jay.jacobs@verizon.com>
2013-07-19 16:21:26 -0500

    github is irritating me.

--- bob-chapters/chapter3/ch3/R/ch3.R
+++ bob-chapters/chapter3/ch3/R/ch3.R
@@ -23,7 +23,7 @@ avRep <- "data/reputation.data"
 # we run the script
 
 if (file.access(avRep)) {
-  download.file(avURL,avRep) 
+  download.file(avURL, avRep) 
 }
 
 # read in the IP reputation db into a data frame
@@ -35,121 +35,125 @@ av <- read.csv(avRep,sep="#",header=FALSE)
 colnames(av) <- c("IP","Reliability","Risk","Type",
                   "Country","Locale","Coords","x")
 
-# take a quick look at the first 10 rows of data
-head(av,n=10)
+# get an R overview of the data frame with str()
+str(av)
 
-# get an R overview of the data frame
-summary(av)
-
-# as we'll be looking at the data quite a bit, 
-# define a function to combine some common tasks
-# set length=10 to limit output without having
-# to always specify the parameter
-headSummary <- function(x, length=10) {
-  # count and organize the data
-  x.factor <- summary(factor(x)) 
-  # sort the table in descending order
-  x.sorted <- sort(x.factor, decreasing=TRUE) 
-  # show the first length values
-  head(x.sorted, n=length)  
-}
+# take a quick look at the first few rows of data
+head(av)
 
 # get an overview of each individual column
-summary(factor(av$Reliability))
-summary(factor(av$Risk))
-headSummary(av$Type)
-headSummary(av$Country,length=Inf)
+summary(av$Reliability)
+summary(av$Risk)
+
+# since these are integers, we can create a table()
+table(av$Reliability)
+table(av$Risk)
+
+# summary sorts by the counts by default
+# maxsum sets how many factors to display
+summary(av$Type, maxsum=10)
+summary(av$Country, maxsum=40)
 
 
 # Bar graph of counts (sorted) by Country (top 20)
 # get the top 20 countries' names
-country.top20 <- names(headSummary(av$Country,length=20))
-# create a subset of the av data frame by selecting only the top
-# 20 countries (by name)
-# tell ggplot what data we are using
-g <- ggplot(data=subset(av,Country %in% country.top20))
+country.top20 <- names(summary(av$Country))[1:20]
+
+# give ggplot a subset of our data (the top 20 countries) 
+# map the x value to a sorted count of country
+g <- ggplot(data=subset(av,Country %in% country.top20), 
+            aes(x=reorder(Country,Country,length)))
 # tell ggplot we want a bar chart and to sort by country count
-g <- g + geom_bar(aes(reorder(Country,Country,length)))
+g <- g + geom_bar()
 # ensure we have decent labels
-g <- g + labs(title="Country Counts",x="Country")
+g <- g + labs(title="Country Counts", x="Country")
 # rotate the chart to make this one more readable
 g <- g + coord_flip()
-g
+print(g)
 
 # Bar graph of counts by Risk
-g <- ggplot(data=av[])
-g <- g + geom_bar(aes(Risk))
+# note we can call ggplot and add the bar chart in one line
+g <- ggplot(data=av, aes(x=Risk)) + geom_bar()
 # force an X scale to be just the limits of the data
 # and to be discrete vs continuous
-g <- g + scale_x_discrete(limits=sequence(range(av$Risk)))
-g <- g + labs(title="'Risk' Counts",x="Risk Score")
-g
+g <- g + scale_x_discrete(limits=seq(max(av$Risk)))
+g <- g + labs(title="'Risk' Counts", x="Risk Score")
+print(g)
 
 # Bar graph of counts by Reliability
-g <- ggplot(data=av)
-g <- g + geom_bar(aes(Reliability))
-g <- g + scale_x_discrete(limits=sequence(range(av$Reliability)))
-g <- g + labs(title="'Reliabiity' Counts",x="Reliability Score")
-g
+g <- ggplot(data=av, aes(x=Reliability)) + geom_bar()
+g <- g + scale_x_discrete(limits=seq(max(av$Reliability)))
+g <- g + labs(title="'Reliabiity' Counts", x="Reliability Score")
+print(g)
 
 
-# make a table out of the Country factor
-country.table = table(av$Country)
-# get the top 10 countries
-top10 = names(sort(country.table,decreasing=TRUE))[1:10]
-# calculate the % for each of the top 5
-sapply(top10,function(x) {
-  country.table[names(country.table)==x]/length(av$Country)
-})
+# store the top 10 returned by summary() in a vector
+country10 <- summary(av$Country, maxsum=10)
+# now convert to a percentage by dividing by number of rows 
+country.perc10 <- country10/nrow(av)
+# and print it
+print(country.perc10)
 
 
 # cross-tabulate the Risk & Reliability factors
 rr.tab <- xtabs(~Risk+Reliability, data=av)
 ftable(rr.tab) # print table
 
-# graphica view
+# graphical view
 library(lattice)
+# cast the table into a data frame
 rr.df = data.frame(table(av$Risk, av$Reliability))
-levelplot(Freq~Var2*Var1, data=rr.df, main="Risk ~ Reliabilty", 
+# set the column names since table uses "Var1" and "Var2"
+colnames(rr.df) <- c("Risk", "Reliability", "Freq")
+# now create a level plot with readable labels
+levelplot(Freq~Risk*Reliability, data=rr.df, main="Risk ~ Reliabilty", 
           ylab="Reliability", xlab = "Risk", shrink = c(0.5, 1),
-          col.regions = colorRampPalette(c("#FFFFFF","#0868AC"))(20))
+          col.regions = colorRampPalette(c("#FFFFFF", "#0868AC"))(20))
 
 
 # generate random samples for risk & reliability and re-run xtab
-rsk=sample(1:7,260000,replace=T)
-rel=sample(1:10,260000,replace=T)
+# starting PRNG from reproducable point
+set.seed(1492) # as it leads to discovery
+# generate 260,000 random samples
+rsk=sample(1:7, 260000, replace=T)
+rel=sample(1:10, 260000, replace=T)
+# cast table into data frame
 tmp.df = data.frame(table(factor(rsk), factor(rel)))
-levelplot(Freq~Var2*Var1, data=tmp.df, main="Risk ~ Reliabilty", 
-          ylab="Reliability", xlab = "Risk", shrink = c(0.5, 1),
-          col.regions = colorRampPalette(c("#FFFFFF","#0868AC"))(20))
+colnames(tmp.df) <- c("Risk", "Reliability", "Freq")
+levelplot(Freq~Reliability*Risk, data=tmp.df, main="Risk ~ Reliabilty", 
+          ylab="Risk", xlab = "Reliability", shrink = c(0.5, 1),
+          col.regions = colorRampPalette(c("#FFFFFF", "#0868AC"))(20))
 
-# create a new "Type" from nodes with multipe Types
-av$newtype <- as.character(av$Type)
-av$newtype[grep(';', av$newtype)] <- "Multiples"
-av$newtype = factor(av$newtype)
-rrt.df = data.frame(table(av$Risk, av$Reliability, av$newtype))
+# create a new varible called "simpletype" 
+# replacing mutiple categories with label of "Multiples"
+av$simpletype <- as.character(av$Type)
+# Group all nodes with mutiple categories into a new category
+av$simpletype[grep(';', av$simpletype)] <- "Multiples"
+# Turn it into a factor again
+av$simpletype <- factor(av$simpletype)
 
-levelplot(Freq ~ Var2*Var1|Var3, data =rrt.df, 
+rrt.df = data.frame(table(av$Risk, av$Reliability, av$simpletype))
+colnames(rrt.df) <- c("Risk", "Reliability", "simpletype", "Freq")
+levelplot(Freq ~ Reliability*Risk|simpletype, data =rrt.df, 
           main="Risk ~ Reliabilty | Type", ylab = "Risk",
           xlab = "Reliability", shrink = c(0.5, 1), 
           col.regions = colorRampPalette(c("#FFFFFF","#0868AC"))(20))
 
-# fiter out "scanning host"s
-rrt.df = subset(rrt.df, Var3 != "Scanning Host")
-rrt.df$Var3 = factor(rrt.df$Var3)
-levelplot(Freq ~ Var2*Var1|Var3, data =rrt.df, 
+# from the existing rrt.df, filter out ‘Scanning Host’
+rrt.df <- subset(rrt.df, simpletype != "Scanning Host")
+levelplot(Freq ~ Reliability*Risk|simpletype, data =rrt.df, 
           main="Risk ~ Reliabilty | Type", ylab = "Risk",
           xlab = "Reliability", shrink = c(0.5, 1), 
           col.regions = colorRampPalette(c("#FFFFFF","#0868AC"))(20))
 
 # filter out malware dist & domain
 rrt.df = subset(rrt.df, 
-           !(Var3 %in% c("Malware distribution", "Malware Domain")))
-rrt.df$Var3 = factor(rrt.df$Var3)
-sprintf("Count: %d; Percent: %2.1f%%",
-        sum(rrt.df$Freq),
-        100*sum(rrt.df$Freq)/nrow(av))
-levelplot(Freq ~ Var2*Var1|Var3, data =rrt.df, 
+           !(simpletype %in% c("Malware distribution", 
+                               "Malware Domain")))
+print(sprintf("Count: %d; Percent: %2.1f%%",
+              sum(rrt.df$Freq),
+              100*sum(rrt.df$Freq)/nrow(av)))
+levelplot(Freq ~ Reliability*Risk|simpletype, data =rrt.df, 
           main="Risk ~ Reliabilty | Type", ylab = "Risk",
           xlab = "Reliability", shrink = c(0.5, 1), 
           col.regions = colorRampPalette(c("#FFFFFF","#0868AC"))(20))
