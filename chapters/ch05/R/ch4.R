58e853ecd3af2eb27b7c4d374789df646ce9bcb5
Jay Jacobs <jay.jacobs@verizon.com>
2013-07-21 09:37:12 -0500

    sunday morning

--- chapters/ch05/R/ch4.R
+++ chapters/ch05/R/ch4.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 # read the CSV with headers
 za <- read.csv("data/zeroaccess.csv", header=T)
 
@@ -9,16 +10,24 @@ gg <- gg + geom_point(size=1, color="#000099", alpha=1/20)
 gg <- gg + xlab("Longitude") + ylab("Latitude")
 # simplify the theme to see points easier
 gg <- gg + theme_bw() 
+gg <- gg + coord_map("rectangular", 0)
 print(gg)
+
+# projections do ?mapproj
 
 # now grab the "world" data
 world <- map_data("world")
-# and strip out antarctica
+# nothing personal penguins, but strip out antarctica
 world <- subset(world, world$region!="Antarctica")
-# projections do ?mapproj
-# mercator, sinusoidal, cylindrical, mollweide, gilbert
+# load world data into ggplot object
 gg <- ggplot(data=world, aes(x=long, y=lat, group=group))
-gg <- gg + geom_path(colour="#CCCCCC") + coord_map("mercator")
-gg <- gg + scale_x_continuous(limits = c(-200, 200)) + # weird fix for linex across the map
-gg <- gg + geom_point(data=za, aes(long, lat), colour="#00009902", size=1.5) + theme_plain()
+# trace along the lat/long coords
+gg <- gg + geom_path(colour="gray70")
+# now project using the mercator projection
+gg <- gg + coord_map("mercator", xlim=c(-200, 200))
+gg <- gg + theme_bw()
+
+gg <- gg + scale_x_continuous(limits = c(-200, 200)) # weird fix for linex across the map
+gg <- gg + geom_point(data=za, aes(long, lat), colour="#00009902", size=1.5)
+gg <- gg + theme_bw()
 print(gg)
