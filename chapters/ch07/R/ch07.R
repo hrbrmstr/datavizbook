bd39ef8002e779abe022cefc37f109e143ed2b2f
Jay Jacobs <jay.jacobs@verizon.com>
2013-11-02 14:44:54 -0500

    AR Final for Ch 7, including VCDB data

--- chapters/ch07/R/ch07.R
+++ chapters/ch07/R/ch07.R
@@ -1,51 +1,97 @@
+
+# Listing 7-1 #########################################################
+# this is a JSON example in the book
+
+# Listing 7-2 #########################################################
+# this is a JSON example in the book
+
+# Listing 7-3 #########################################################
+# set working directory to chapter location
+# (change for where you set up files in ch 2)
+setwd("~/book/ch07")
+# make sure the packages for this chapter
+# are installed, install if necessary
+pkg <- c("devtools", "ggplot2", "scales", "rjson")
+new.pkg <- pkg[!(pkg %in% installed.packages())]
+if (length(new.pkg)) {
+  install.packages(new.pkg)
+}
+
+# Listing 7-4 #########################################################
+library(rjson)
+# fromJSON accepts a filename to read from
+jsonfile <- "data/vcdb/F58E9169-AC07-400E-AB0E-DB784C6CAE59.json"
+incident <- fromJSON(file=jsonfile)
+# print the hacking variety
+print(incident$action$hacking$variety)
+## [1] "SQLi"
+
+# Listing 7-5 #########################################################
 # if we haven't installed devtools yet, let's install it
 library(devtools)
-
 # now let's install the verisr package
 install_github("verisr", "jayjacobs")
-
 # and load up the library
 library(verisr)
 
-# grab the incidents from the VCDB repository
-# https://github.com/vz-risk/VCDB
-# set the dir to the incidents/ directly of that
-jsondir <- '../VCDB/incidents'
-
+# Listing 7-6 #########################################################
+# requires package : verisr
+# set this to where VCDB incidents are stored
+jsondir <- 'data/vcdb/'
 # create a veris instance with the vcdb data
 vcdb <- json2veris(jsondir)
 
+# Listing 7-6 #########################################################
+# requires package : verisr
+# requires object: vcdb (7-6)
 # we can get some high level overview of the incidents by running
 summary(vcdb)
-plot(vcdb)
+## 1643 incidents in this object.
+##
+## Actor:
+## external internal  partner  unknown
+##      955      535      100       85
+##
+## Action:
+##    error  hacking  malware   misuse physical   social
+##      398      416       42      216      508       31
+##
+## Asset:
+## Kiosk/Term  Media  Network   Person   Server  Unknown   User Dev
+##         17    534        8       33      656       80        429
+##
+## Attribute:
+##  confidentiality     availability  confidentiality     integrity
+##                2              614             1604           165
+#plot(vcdb)
 
-# we should already have verisr loaded and the vcdb object created.
+# Listing 7-7 #########################################################
+# requires package : verisr
+# requires object: vcdb (7-6)
 actors <- getenum(vcdb, "actor")
-
 # actors is a data frame 
 print(actors)
+##       enum   x
+## 1 external 955
+## 2 internal 535
+## 3  partner 100
+## 4  unknown  85
 
-##       enum    x
-## 1 external 1024
-## 2 internal  556
-## 3  partner  104
-## 4  unknown   86
-
-
+# Listing 7-7 #########################################################
+# requires package : verisr
+# requires object: vcdb (7-6)
 actors <- getenum(vcdb, "actor", add.n=TRUE, add.freq=TRUE)
 print(actors)
-## enum    x    n freq
-## 1 external 1024 1737 0.59
-## 2 internal  556 1737 0.32
-## 3  partner  104 1737 0.06
-## 4  unknown   86 1737 0.05
+##       enum   x    n  freq
+## 1 external 955 1643 0.581
+## 2 internal 535 1643 0.326
+## 3  partner 100 1643 0.061
+## 4  unknown  85 1643 0.052
 
-# load ggplot2
+# Listing 7-7 #########################################################
+# requires package : verisr, ggplot2
+# requires object: vcdb (7-6)
 library(ggplot2)
-
-
-## basis for figure 7.5 
-
 # take in the vcdb object and the field to plot
 verisplot <- function(vcdb, field) {
   # get the data.frame for the field with frequency
@@ -58,14 +104,14 @@ verisplot <- function(vcdb, field) {
   gg <- ggplot(localdf, aes(x=enum, y=freq, label=lab))
   gg <- gg + geom_bar(stat="identity", fill="steelblue")
   # add in text, adjusted to the end of the bar
-  gg <- gg + geom_text(hjust=-0.1)
+  gg <- gg + geom_text(hjust=-0.1, size=3)
   # flip the axes and add in a title
   gg <- gg + coord_flip() + ggtitle(field)
   # remove axes labels and add bw theme
   gg <- gg + xlab("") + ylab("") + theme_bw()
   # fix the y scale to remove padding and fit our label (add 7%) 
   gg <- gg + scale_y_continuous(expand=c(0,0), 
-                                limits=c(0, max(localdf$freq)*1.07))
+                                limits=c(0, max(localdf$freq)*1.1))
   # make it slightly prettier than the default
   gg <- gg + theme(panel.grid.major = element_blank(),
                    panel.border = element_blank(),
@@ -73,6 +119,7 @@ verisplot <- function(vcdb, field) {
                    axis.ticks = element_blank())
 }
 
+# try the following (code not in book)
 print(verisplot(vcdb, "action"))
 print(verisplot(vcdb, "actor.external.variety"))
 print(verisplot(vcdb, "action.physical.variety"))
@@ -80,16 +127,17 @@ print(verisplot(vcdb, "action.hacking.vector"))
 print(verisplot(vcdb, "attribute.confidentiality.data.variety"))
 print(verisplot(vcdb, "asset.assets"))
 
-
-# Figures 7.6 and basis for 7.7
-
+# Listing 7-8 #########################################################
+# requires package : verisr, ggplot2
+# requires object: vcdb (7-6)
 # get a data.frame comparing the actions to the assets
 # this will add zero's in missing squares and include a frequency
 a2 <- getenum(vcdb, enum="action", primary="asset.assets", add.freq=T)
 # trim unknown asset and environment action for space
-a2 <- a2[-which(a2$enum=="environmental" | a2$primary=="Unknown"), ]
+a2 <- a2[which(a2$enum!="environmental" & a2$primary!="Unknown"), ]
 # so we should create a "slim" version without zeros to color it
-slim.a2 <- a2[-which(a2$x==0), ]
+slim.a2 <- a2[which(a2$x!=0), ]
+# could sort these by converting to factors (we did in Fig 7-6)
 
 # now make a nice plot  
 gg <- ggplot(a2, aes(x=enum, y=primary, fill=freq))
@@ -106,14 +154,12 @@ print(gg)
 
 
 
-
-########
-## actor section, figure 7.1
-#######
+# Figure 7-1 #########################################################
+# requires package : verisr, ggplot2, scales
+# requires object: vcdb (7-6)
 library(ggplot2)
 library(scales)
 # should have verisr loaded and vcdb object created
-
 # get the count of incidents with an actor
 actors <- getenumlist(vcdb, "actor")
 actfields <- c("external", "internal", "partner")
@@ -136,7 +182,7 @@ finalmotive <- allmotive[-which(allmotive$enum=="Unknown"), ]
 gg <- ggplot(finalmotive, aes(x=enum, y=freq, label=paste(round(freq, 2)*100, "%", sep='')))
 gg <- gg + geom_bar(stat="identity", fill="steelblue") 
 gg <- gg + geom_text(hjust=-0.1)
-gg <- gg + scale_y_continuous(labels=percent, expand=c(0,0), limits=c(0, max(allmotive$freq)+.02)) 
+gg <- gg + scale_y_continuous(labels=percent, expand=c(0,0), limits=c(0, max(allmotive$freq)+.03)) 
 gg <- gg + coord_flip()
 gg <- gg + xlab("") + ylab("") # title in book caption
 gg <- gg + theme(axis.text.x = element_blank(),
@@ -150,10 +196,9 @@ gg <- gg + theme(axis.text.x = element_blank(),
 print(gg)
 
 
-
-########
-## Action section: Figure 7.2
-#######
+# Figure 7-2 #########################################################
+# requires package : verisr, ggplot2, scales
+# requires object: vcdb (7-6)
 library(ggplot2)
 library(scales)
 
@@ -195,9 +240,9 @@ gg <- gg + theme(axis.text.x = element_blank(),
 print(gg)
 
 
-########
-## Asset section: Figure 7.3
-#######
+# Figure 7-3 #########################################################
+# requires package : verisr, ggplot2, scales
+# requires object: vcdb (7-6)
 library(ggplot2)
 library(scales)
 # should have verisr loaded and vcdb object created
@@ -207,7 +252,6 @@ asset <- getenum(vcdb, "asset.assets", add.freq=T)
 # flip it
 asset <- asset[(with(asset, order(x))), ]
 asset$enum <- factor(asset$enum, levels=asset$enum, ordered=T)
-#levels(asset$enum) <- rev(levels(asset$enum))
 
 gg <- ggplot(asset, aes(x=enum, y=freq, label=paste(round(freq, 2)*100, "%", sep='')))
 gg <- gg + geom_bar(stat="identity", fill="steelblue") 
@@ -225,10 +269,9 @@ gg <- gg + theme(axis.text.x = element_blank(),
                  panel.background = element_blank())
 print(gg)
 
-
-########
-## Victim section, figure 7.4
-#######
+# Figure 7-4 #########################################################
+# requires package : verisr, ggplot2, scales
+# requires object: vcdb (7-6)
 library(ggplot2)
 library(scales)
 # should have verisr loaded and vcdb object created
@@ -261,3 +304,17 @@ gg <- gg + theme(axis.text.x = element_blank(),
                  panel.border = element_blank(),
                  panel.background = element_blank())
 print(gg)
+
+
+# Figure 7-4 #########################################################
+# requires package : verisr, ggplot2, scales, gridExtra
+# requires object: vcdb (7-6), verisplot (7-7)
+library(gridExtra)
+aa <- verisplot(vcdb, "action")
+bb <- verisplot(vcdb, "actor.external.variety")
+cc <- verisplot(vcdb, "action.physical.variety")
+dd <- verisplot(vcdb, "action.hacking.vector")
+ee <- verisplot(vcdb, "attribute.confidentiality.data.variety")
+ff <- verisplot(vcdb, "asset.assets")
+grid.arrange(aa, bb, cc, dd, ee, ff, ncol=2, clip=T)
+
